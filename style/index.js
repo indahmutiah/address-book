46d5bd27c696e9c5b33b4ad0573b1d273f7bdc80
Indah Mutiah Utami MZ <indahmutiahmz@gmail.com>
2024-12-05 20:39:56 +0700

    add javascript

---
+++ style/index.js
@@ -0,0 +1,13 @@
+const contacts = [
+  {
+    name: "John Doe",
+    email: "johndoe@example.com",
+    phone: "555-123-4567",
+  },
+  {
+    name: "Jane Smith",
+    email: "janesmith@example.com",
+    phone: "555-234-5678",
+  },
+];
+console.log("List of contacs",contacts);
