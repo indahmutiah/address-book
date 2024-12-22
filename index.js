28e425fe72efaefa36ea133e5f47026eb7f0f47b
Indah Mutiah MZ <indah@cnt.id>
2024-12-22 12:54:08 +0700

    fixing bugs

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let dataContacts = [
     phone: "+62 812-1229-5678",
     birthdate: new Date("1998-06-28"),
     isFavorited: true,
-    label: ["Family"],
+    label: "Family",
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ let dataContacts = [
     phone: "+62 878-7889-5678",
     birthdate: new Date("1989-10-20"),
     isFavorited: false,
-    label: ["Friend", "Work"],
+    label: "Work",
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ let dataContacts = [
     phone: "+62 889-3529-5678",
     birthdate: new Date("1993-03-13"),
     isFavorited: false,
-    label: ["Others"],
+    label: "Others",
   },
   {
     id: 4,
@@ -33,7 +33,7 @@ let dataContacts = [
     phone: "+62 890-1234-5678",
     birthdate: new Date("1996-04-30"),
     isFavorited: true,
-    label: ["Friend", "Work"],
+    label: "Friend",
   },
   {
     id: 5,
@@ -42,30 +42,25 @@ let dataContacts = [
     phone: "+62 891-2345-5678",
     birthdate: new Date("2003-12-09"),
     isFavorited: true,
-    label: ["Family", "Others"],
+    label: "Family",
   },
 ];
 
-saveToLocalStorage(dataContacts);
+// Save Contacts to Local Storage
+function saveToLocalStorage(contacts) {
+  localStorage.setItem("contacts", JSON.stringify(contacts));
+}
 
 function getContactsFromLocalStorage() {
-  const storedData = localStorage.getItem("contacts");
-  if (storedData) {
-    return JSON.parse(storedData);
-  } else {
-    console.log("No contacts found in localStorage.");
-    return []; // Return an empty array if no contacts are found
-  }
+  return JSON.parse(localStorage.getItem("contacts")) || [];
 }
-
-// Save Contacts to Local Storage
-function saveToLocalStorage(dataContacts) {
-  localStorage.setItem("contacts", JSON.stringify(dataContacts));
-}
-
-const contactListElement = document.getElementById("contact-list");
 
 function renderContacts(contacts) {
+  const contactListElement = document.getElementById("contact-list");
+  if (!contactListElement) {
+    console.warn("Element with id 'contact-list' not found. Skipping rendering.");
+    return;
+  }
   const contactsTableRowElements = contacts.map((contact) => {
     const formattedPhone = contact.phone.replace(/-/g, "");
     const formattedDate = new Intl.DateTimeFormat("en-UK", {
@@ -81,8 +76,12 @@ function renderContacts(contacts) {
         <td class="p-4 border-b">${formattedPhone}</td>
         <td class="p-4 border-b">${contact.email}</td>
         <td class="p-4 border-b">${formattedDate}</td>
-        <td class="p-4 border-b">${contact.label.join(", ")}</td>
-        <td class="p-4 border-b">${contact.isFavorited ? "★" : ""}</td>
+        <td class="p-4 border-b">${contact.label || ""}</td>
+        <td class="p-4 border-b">${
+          contact.isFavorited
+            ? '<i class="fas fa-star text-yellow-400"></i>'
+            : ""
+        }</td>
         <td class="p-4 border-b">
           <button
             class="flex-col items-center justify-center w-8 h-8 rounded-lg bg-blue-500 border border-gray-300 hover:bg-blue-400 hover:border-gray-400"
@@ -118,46 +117,11 @@ function renderOneContact(contacts, contactId) {
   renderContacts([contact]);
 }
 
-// Searching Function
-function searchContacts(contact, searchTerm) {
-  const searchingContacts = contact.filter((contact) => {
-    return (
-      contact.name
-        .toLocaleLowerCase()
-        .includes(searchTerm.toLocaleLowerCase()) ||
-      contact.email
-        .toLocaleLowerCase()
-        .includes(searchTerm.toLocaleLowerCase()) ||
-      contact.phone
-        .replace(/-/g, "")
-        .toLocaleLowerCase()
-        .includes(searchTerm.toLocaleLowerCase()) ||
-      new Intl.DateTimeFormat("en-UK", {
-        dateStyle: "long",
-        timeStyle: "short",
-      })
-        .format(contact.birthdate)
-        .toLocaleLowerCase()
-        .includes(searchTerm.toLocaleLowerCase()) ||
-      (contact.isFavorited
-        ? "★".includes(searchTerm.toLocaleLowerCase())
-        : false) ||
-      contact.label.some((label) =>
-        label.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
-      )
-    );
-  });
-  if (searchingContacts.length === 0) {
-    console.log("Not Found");
-  } else {
-    renderContacts(searchingContacts);
-  }
-}
-
 // Add Contact Function
 function generateId(contacts) {
   return contacts[contacts.length - 1].id + 1;
 }
+
 function addContact(contacts, newContactInput) {
   const newContact = {
     id: generateId(contacts),
@@ -175,60 +139,98 @@ function addContact(contacts, newContactInput) {
   renderContacts(newContacts);
 }
 
-// Delete Function
-function deleteContact(contacts, contactId) {
-  const filteredContacts = contacts.filter((contact) => {
-    return contact.id !== contactId;
-  });
-  saveToLocalStorage(filteredContacts);
-  renderContacts(filteredContacts);
-}
-
-// Update Contact Function
-function updateContact(contacts, contactId, updatedContactInput) {
-  const currentContact = contacts.find((contact) => {
-    return contact.id === contactId;
-  });
-  const updatedContact = {
-    id: contactId,
-    name: updatedContactInput.name || currentContact.name,
-    email: updatedContactInput.email || currentContact.email,
-    phone: updatedContactInput.phone || currentContact.phone,
-    birthdate: new Date(
-      updatedContactInput.birthdate || currentContact.birthdate
-    ),
-    isFavorited: updatedContactInput.isFavorited || currentContact.isFavorited,
-    label: updatedContactInput.label || currentContact.label,
-  };
-  const updatedContacts = contacts.map((contact) => {
-    if (contact.id === contactId) {
-      return updatedContact;
-    }
-    return contact;
-  });
-  saveToLocalStorage(updatedContacts);
-  renderContacts(updatedContacts);
-}
-
-// Searching Contacts
-searchContacts(dataContacts, "sarah");
-
-// Add Contact
-addContact(dataContacts, {
-  name: "Joko Mulyono",
-  email: "joko@example.com",
-  phone: "+62 890-1234-5678",
-  birthdate: new Date("1988-04-30"),
-  isFavorited: true,
-  label: ["Work"],
-});
-
-// Update Contact
-updateContact(dataContacts, 2, {
-  email: "salahuddin12@example.com",
-});
-
 function totalContacts() {
   return dataContacts.length;
 }
 console.log("Total Contacts: ", totalContacts());
+
+renderContacts(dataContacts);
+
+// // Searching Function
+// function searchContacts(contact, searchTerm) {
+//   const searchingContacts = contact.filter((contact) => {
+//     return (
+//       contact.name
+//         .toLocaleLowerCase()
+//         .includes(searchTerm.toLocaleLowerCase()) ||
+//       contact.email
+//         .toLocaleLowerCase()
+//         .includes(searchTerm.toLocaleLowerCase()) ||
+//       contact.phone
+//         .replace(/-/g, "")
+//         .toLocaleLowerCase()
+//         .includes(searchTerm.toLocaleLowerCase()) ||
+//       new Intl.DateTimeFormat("en-UK", {
+//         dateStyle: "long",
+//         timeStyle: "short",
+//       })
+//         .format(contact.birthdate)
+//         .toLocaleLowerCase()
+//         .includes(searchTerm.toLocaleLowerCase()) ||
+//       (contact.isFavorited
+//         ? "★".includes(searchTerm.toLocaleLowerCase())
+//         : false) ||
+//       contact.label.some((label) =>
+//         label.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
+//       )
+//     );
+//   });
+//   if (searchingContacts.length === 0) {
+//     console.log("Not Found");
+//   } else {
+//     renderContacts(searchingContacts);
+//   }
+// }
+
+// // Update Contact Function
+// function updateContact(contacts, contactId, updatedContactInput) {
+//   const currentContact = contacts.find((contact) => {
+//     return contact.id === contactId;
+//   });
+//   const updatedContact = {
+//     id: contactId,
+//     name: updatedContactInput.name || currentContact.name,
+//     email: updatedContactInput.email || currentContact.email,
+//     phone: updatedContactInput.phone || currentContact.phone,
+//     birthdate: new Date(
+//       updatedContactInput.birthdate || currentContact.birthdate
+//     ),
+//     isFavorited: updatedContactInput.isFavorited || currentContact.isFavorited,
+//     label: updatedContactInput.label || currentContact.label,
+//   };
+//   const updatedContacts = contacts.map((contact) => {
+//     if (contact.id === contactId) {
+//       return updatedContact;
+//     }
+//     return contact;
+//   });
+//   saveToLocalStorage(updatedContacts);
+//   renderContacts(updatedContacts);
+// }
+
+// // Delete Function
+// function deleteContact(contacts, contactId) {
+//   const filteredContacts = contacts.filter((contact) => {
+//     return contact.id !== contactId;
+//   });
+//   saveToLocalStorage(filteredContacts);
+//   renderContacts(filteredContacts);
+// }
+
+// // Searching Contacts
+// searchContacts(dataContacts, "sarah");
+
+// // Add Contact
+// addContact(dataContacts, {
+//   name: "Joko Mulyono",
+//   email: "joko@example.com",
+//   phone: "+62 890-1234-5678",
+//   birthdate: new Date("1988-04-30"),
+//   isFavorited: true,
+//   label: ["Work"],
+// });
+
+// // Update Contact
+// updateContact(dataContacts, 2, {
+//   email: "salahuddin12@example.com",
+// });
